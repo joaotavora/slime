2264bff955e87a71da0425d3e42d84c263b6a9df
Joao Tavora <joaotavora@gmail.com>
2014-02-23 21:05:02 +0000

    Closes #105: Port indentation tests to use ERT
    * contrib/test/slime-cl-indent-test.txt: Moved to from previous file and
    changed test names from numbers to symbols.
    
    * contrib/test/slime-indentation-tests.el: New file. Defines ERT
    indentation tests from specs in `slime-cl-indent-test.txt'.
    
    * contrib/slime-cl-indent.el: Moved tests to
    test/slime-indentation-tests.el. In file comments, explain the
    legacy cl-indent.el situation. Provide `slime-cl-indent'.
    
    * contrib/slime-indentation.el (slime-cl-indet): `require' instead of
    `load'.

--- contrib/slime-indentation.el
+++ contrib/slime-indentation.el
@@ -1,11 +1,10 @@
 (eval-and-compile
   (require 'slime))
+(require 'slime-cl-indent)
 
 (define-slime-contrib slime-indentation
   "Patched version of cl-indent.el as a slime-contrib module"
   (:swank-dependencies swank-indentation))
-
-(load "slime-cl-indent.el")
 
 (setq common-lisp-current-package-function 'slime-current-package)
 
